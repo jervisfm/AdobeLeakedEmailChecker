8cd0b59fd0f534d3d698ce02d9fd42ad2f800be5
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-12-15 08:33:57 +0000

    Add more program flags

--- alec.cc
+++ alec.cc
@@ -8,15 +8,28 @@
 // Custom Common Code
 #include "common/base/flags.h"
 #include "common/base/init.h"
+#include "common/log/log.h"
 
 // Program Flags
-DEFINE_string(db_path, "adobe.db", 
+DEFINE_string(file_path, "adobe.db", 
 	      "Path to LevelDB file containing leaked Adobe passwords."
 	      "Defaults to using 'adobe.db'");
-
+DEFINE_string(dump_file, "adobe_dump.txt", 
+	      "File path to the uncompressed raw dump of the adobe credentials.");
+DEFINE_bool(process_raw_dump, false, "Assumes that the file path "
+	    "in '--dump_file' points to a raw text dump of the credentials and process them"
+	    "to generate an on disk LEVELDB hashtable with the name specified in '--output_file'."
+            "The LEVELDB hastable will be queryable in O(1) / constant time. ");
 using namespace std;
-int main(int argc, char**argv) {
+
+static void OutputFlags() {
+  LOG(INFO) << "(LevelDB) file_path: " << FLAGS_file_path;
+  LOG(INFO) << "dump_file: " << FLAGS_dump_file;
+  LOG(INFO) << "process_raw_dump?" << FLAGS_process_raw_dump; 
+}
+
+int main(int argc, char **argv) {
   InitProgram(&argc, &argv);
-  cout << "DB PathFlag is " << FLAGS_db_path;
+  OutputFlags();
   return 0;
 }
