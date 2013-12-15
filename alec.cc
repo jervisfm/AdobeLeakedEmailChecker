edd8a28807d4f2febf0a4b348dfc0b9a3b0811c6
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-12-15 08:10:42 +0000

    Add a test flag

--- alec.cc
+++ alec.cc
@@ -1,9 +1,22 @@
+// Adobe Leaked Email Chcker
+// Date: December 2013
+// Author: Jervis Muindi
 
-
+// Standard Lib Header
 #include <iostream>
+
+// Custom Common Code
+#include "common/base/flags.h"
+#include "common/base/init.h"
+
+// Program Flags
+DEFINE_string(db_path, "adobe.db", 
+	      "Path to LevelDB file containing leaked Adobe passwords."
+	      "Defaults to using 'adobe.db'");
 
 using namespace std;
 int main(int argc, char**argv) {
-  cout << "Hello" << endl;
+  InitProgram(&argc, &argv);
+  cout << "DB PathFlag is " << FLAGS_db_path;
   return 0;
 }
