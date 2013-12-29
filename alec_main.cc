dc072d378eb8d75d6ac04d18d9ff4e0372be02db
Jervis Muindi <jervismuindi@yahoo.co.uk>
2013-12-29 08:45:54 +0000

    working on testing the save to on disk hashtable code

--- alec_main.cc
+++ alec_main.cc
@@ -12,18 +12,18 @@
 
 #include "alec.h"
 
+using namespace alec;
 
 // Program Flags
 DEFINE_string(file_path, "adobe.db", 
-	      "Path to LevelDB file containing leaked Adobe passwords."
+	      "Path to processed LevelDB file containing leaked Adobe passwords."
 	      "Defaults to using 'adobe.db'");
 DEFINE_string(dump_file, "adobe_dump.txt", 
 	      "File path to the uncompressed raw dump of the adobe credentials.");
 DEFINE_bool(process_raw_dump, false, "Assumes that the file path "
 	    "in '--dump_file' points to a raw text dump of the credentials and process them"
-	    "to generate an on disk LEVELDB hashtable with the name specified in '--output_file'."
-            "The LEVELDB hastable will be queryable in O(1) / constant time. ");
-
+	    "to generate an on disk LEVELDB hashtable with the name specified in '--file_path'."
+            "The LEVELDB hastable will be queryable in O(1) / constant time.");
 
 static void OutputFlags() {
   LOG(INFO) << "(LevelDB) file_path: " << FLAGS_file_path;
@@ -31,11 +31,22 @@ static void OutputFlags() {
   LOG(INFO) << "process_raw_dump?" << FLAGS_process_raw_dump; 
 }
 
-
+static bool ProcessRawDump() {
+  if (!FLAGS_process_raw_dump) { 
+    return false; 
+  }
+  CredentialReader reader(FLAGS_dump_file);
+  CredentialProcessor processor(&reader);
+  bool success = processor.GenerateDiskHashTable(FLAGS_file_path);
+  return success;
+}
 
 int main(int argc, char **argv) {
   InitProgram(&argc, &argv);
   LOG(INFO) << "Hello from Alec MAIN" ;
   OutputFlags();
+  if (FLAGS_process_raw_dump) {
+    return ProcessRawDump(); 
+  }
   return 0;
 }
